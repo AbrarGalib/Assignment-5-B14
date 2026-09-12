6eb738b2b2358406db89de0fb4d2020f7ccd9e8f
AbrarGalib <abrargalib00@gmail.com>
2026-09-12 13:35:50 +0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+
+function App() {
+
+  return (
+    <>
+      <h2 className="text-2xl font-bold">Hello, Vite + React!</h2>
+      <button className="btn btn-error">Error</button>
+    </>
+  )
+}
+
+export default App
