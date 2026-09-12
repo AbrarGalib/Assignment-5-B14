884725c239a789d2462f0d7612945d885aa34999
AbrarGalib <abrargalib00@gmail.com>
2026-09-12 21:51:30 +0600

    navbar added

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
+import Navbar from "./components/Navbar"
 
 
 function App() {
 
   return (
     <>
-      <h2 className="text-2xl font-bold">Hello, Vite + React!</h2>
-      <button className="btn btn-error">Error</button>
+      <Navbar />
     </>
   )
 }
